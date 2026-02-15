13dd0fbd559fa01159a51c89d4422805c215b906
David Vilar <david.vilar@gmail.com>
2026-02-15 17:25:10 +0100

    Niri: focus_window looks only in the current workspace

--- .config/niri/focus_window.py
+++ .config/niri/focus_window.py
@@ -2,8 +2,8 @@
 
 import functools
 import json
-import sys
 import subprocess
+import sys
 from typing import Any
 
 
@@ -11,6 +11,7 @@ def niri_cmd(command: str):
   process_stats = subprocess.run(
     ["niri", "msg", "--json"] + command.split(),
     capture_output=True,
+    text=True,
   )
   return process_stats
 
@@ -20,20 +21,27 @@ def window_cmp(w1: dict[str, Any], w2: dict[str, Any]) -> int:
   col2, row2 = w2['layout']['pos_in_scrolling_layout']
   if col1 < col2:
     return -1
-  elif col2 > col1:
+  elif col1 > col2:
     return 1
   elif row1 < row2:
     return -1
-  elif row2 > row1:
+  elif row1 > row2:
     return 1
   else:
     return 0  # Shouldn't happen
+
 
 def main():
   window_nr = int(sys.argv[1]) - 1
 
   niri_out = niri_cmd("windows")
-  window_list = json.loads(niri_out.stdout.decode())
+  window_list = json.loads(niri_out.stdout)
+  focused_window = [w for w in window_list if w["is_focused"]]
+  if not focused_window:
+    return
+  focused_ws = focused_window[0]["workspace_id"]
+  window_list = [w for w in window_list if w["workspace_id"] == focused_ws]
+  print(80*"-")
   window_list.sort(key=functools.cmp_to_key(window_cmp))
   if window_nr >= len(window_list):
     return
