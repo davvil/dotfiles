a8ca432b178bff9d387c819dac1a4ff3527cb716
David Vilar <david.vilar@gmail.com>
2026-06-07 17:00:46 +0200

    Added missing ws-icons.py

---
+++ .config/sway/ws_icons.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import sys
+
+
+known_icons = {
+    "web": "󰖟",
+    "General": "",
+}
+default_icon = ""
+
+
+def get_ws_icon(name):
+  return known_icons.get(name, default_icon)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        print(get_ws_icon(sys.argv[1]))
